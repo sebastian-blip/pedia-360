3d2fa4a8aef08ddb0af068ad5298ac83b225f38e
sebastian <sebastian.betancourth@yuju.io>
2026-08-09 22:31:00 -0500

    fix: eliminar precios del webinar

--- dist/server/entry.mjs
+++
@@ -1,8 +0,0 @@
-export { p as handler, q as options, t as startServer } from './chunks/server_wOc8bkzF.mjs';
-import '@astrojs/internal-helpers/path';
-import '@astrojs/internal-helpers/remote';
-import 'piccolore';
-import 'es-module-lexer';
-import 'clsx';
-import '@astrojs/internal-helpers/object';
-import 'html-escaper';
